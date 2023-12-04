757fb4335f8f190e2fa64171c1edf1d8a0ac3f79
mtbrr26 <breiermathieu@gmail.com>
2023-12-04 16:51:16 +0100

    3

--- folder_lib/used_car_prediction_lib/features/encoder.py
+++ folder_lib/used_car_prediction_lib/features/encoder.py
@@ -1,12 +1,11 @@
 from abc import ABCMeta, abstractmethod
 import pandas as pd
-import numpy as np
 
 ####################father################   
 class Encoder(metaclass = ABCMeta):
     
     @abstractmethod
-    def encoding_process(X_train, y_train, X_test, y_test):
+    def encoding_process(df, columns_to_encode):
         return NotImplementedError
 
 
@@ -18,7 +17,7 @@ class BinaryEncoder(Encoder):
         self.false_value = false_value
         
     #function for binary variables where we specify the true and false values
-    def encoding_process(self, df, column_names: list):
+    def encoding_process(self, df, column_names):
         for column_name in column_names:
             df[column_name] = df[column_name].replace({self.true_value: 1, self.false_value: 0})
         return df
--- folder_lib/used_car_prediction_lib/features/graphics.py
+++ folder_lib/used_car_prediction_lib/features/graphics.py
@@ -1,33 +1,49 @@
 from abc import ABCMeta, abstractmethod
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 ####################father################   
 class Graphics(metaclass = ABCMeta):
     
     @abstractmethod
-    def plotting_graphics(X_train, y_train, X_test, y_test):
+    def plotting(df,columns):
         return NotImplementedError
 
+####################child################   
+class BoxPlotsGraphics(Graphics): 
+
+    def plotting(df, columns):
+        fig, ax = plt.subplots(figsize=(10, 6))
+        df.boxplot(column=columns, ax=ax)
+        plt.title('Boxplot')
+        plt.ylabel('Values')
+        plt.xlabel('Columns')
+        plt.xticks(rotation=45)
+        plt.show()
 
 ####################child################   
-class BinaryEncoder(Encoder): 
+class DistributionGraphics(Graphics): 
+    def plotting(df, columns):
+        num_cols = len(columns)
+        num_rows = (num_cols // 2) + (num_cols % 2)
+        fig, axes = plt.subplots(num_rows, 2, figsize=(15, num_rows * 5))
+        axes = axes.flatten()
 
-    #function for binary variables where we specify the true and false values
-    def encoding_process(df, column_names: list, true_value, false_value):
-        for column_name in column_names:
-            df[column_name] = df[column_name].replace({true_value: 1, false_value: 0})
-        return df
+        for i, col in enumerate(columns):
+            ax = axes[i]
+            ax.hist(df[col], bins=30, alpha=0.7)
+            ax.set_title(f'Distribution of {col}')
+            ax.set_xlabel(col)
+            ax.set_ylabel('Density')
+
+        plt.tight_layout()
+        plt.show()
 
 ####################child################   
-class CategoryOneHotEncoder(Encoder): 
-    #function for categorical variables 
-    def encoding_process(df, columns_to_encode):
-        df = pd.get_dummies(df, columns=columns_to_encode, drop_first=True)
-            
-        # Convert Boolean columns to integers (0s and 1s)
-        boolean_columns = df.select_dtypes(include='bool').columns
-        df[boolean_columns] = df[boolean_columns].astype(int)
-            
-        return df
-
+class CorrelationMatrixGraphics(Graphics): 
+    def plotting(df, columns):
+        corr = df[columns].corr()
+        plt.figure(figsize=(10, 8))
+        heatmap = sns.heatmap(corr, annot=True, cmap='viridis', fmt=".2f")
+        heatmap.set_title('Correlation Matrix', fontdict={'fontsize': 18})
+        plt.show()
